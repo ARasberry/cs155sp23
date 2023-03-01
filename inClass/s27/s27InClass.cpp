7fdfe45d0cace29b77c0656afa46705ccb4fafa4
Austin Rasberry <97619240+ARasberry@users.noreply.github.com>
2023-03-01 11:29:59 -0600

    Create s27InClass.cpp

---
+++ inClass/s27/s27InClass.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int grade;
+    double sum = 0;
+    double avg;
+    int count = 0;
+    cout <<"Enter grades. Type a negative number when finished." << endl;
+
+    while (cin >> grade && grade >= 0){
+    sum += grade;
+    count++;
+    }
+    if (count > 0){
+       avg = sum/count;
+       cout << avg << endl;
+    }
+    else{
+         avg = 0;
+         cout << "Error no grades given. Cannot divide by zero." << endl;
+    }
+
+
+
+
+return 0;
+}
