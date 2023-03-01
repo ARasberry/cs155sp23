6f271f5dea0e4eda6c4ed8a4e252867e84476166
Austin Rasberry <97619240+ARasberry@users.noreply.github.com>
2023-03-01 11:32:29 -0600

    Update s27InClass.cpp

--- inClass/s27/s27InClass.cpp
+++ inClass/s27/s27InClass.cpp
@@ -10,20 +10,17 @@ int main(){
     cout <<"Enter grades. Type a negative number when finished." << endl;
 
     while (cin >> grade && grade >= 0){
-    sum += grade;
-    count++;
+        sum += grade;
+        count++;
     }
     if (count > 0){
-       avg = sum/count;
-       cout << avg << endl;
+        avg = sum/count;
+        cout << avg << endl;
     }
     else{
-         avg = 0;
-         cout << "Error no grades given. Cannot divide by zero." << endl;
+        avg = 0;
+        cout << "Error no grades given. Cannot divide by zero." << endl;
     }
-
-
-
 
 return 0;
 }
