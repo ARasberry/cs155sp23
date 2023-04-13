2682af515fe85bbf50b1e2d4848133074565521d
Austin Rasberry <97619240+ARasberry@users.noreply.github.com>
2023-04-13 11:19:47 -0500

    Create cStringTinkering.cpp

---
+++ inClass/s47/cStringTinkering.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    char ch1 = 'X';
+    char str[5] = {'l', 'i', 'o', 'n', 's'};
+    char ch2 = 'Y';
+    cout << str << endl;
+    cout << (str-1) << endl;
+    cout << (str-2) << endl;
+    return 0;
+}
